c63cdd5aa3e7a511f9e06980213b058d1e8a9a4c
IngDeiv <115591943+IngDeiv@users.noreply.github.com>
2022-10-11 16:41:30 -0500

    Add files via upload

---
+++ style/js/app.js
@@ -0,0 +1,49 @@
+const header = document.querySelector("header");
+
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelector(".counter span");
+const progress_bars = document.querySelector(".skills svg circle");
+
+window.addEventListener("scroll", ()=>{
+    // console.log("he;ll");
+    skillsCounter();
+});
+
+/* ---------------- Sticky Navbar ---------------- */
+
+function stickyNavbar(){
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+    // console.log(window.pageXOffset);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
+/* ---------------- Scroll Reveal ---------------- */
+
+let sr = ScrollReveal({
+    duration: 500,
+    distance: "60px"
+});
+
+// sr.reveal(".showcase-info", { delay:600});
+// sr.reveal(".showcase-image", { origin:"top", delay:600});
+
+/* ---------------- Skill Progress Bar Animation ---------------- */
+
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+    // console.log(topPosition)
+    if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+    return false;
+}
+
+function skillsCounter(){
+    if(!hasReached(first_skill)) return;
+    // console.log("topPosition")
+
+    progress_bars.forEach((p) => (p.style.animation = "progress 2s ease-in-out"));
+}
+
+
+
